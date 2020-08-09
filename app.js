9fec4940ead7ffd0cc5ca08228de33ddcb4c10b3
Raga Pushadwa <ragapushadwa@gmail.com>
2020-08-09 15:24:33 +0700

    push

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const nav = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
 const links = nav.querySelectorAll("a");
+let video1 = $(".gallerybox");
+var activities=document.getElementById("kotak1");
 
 burger.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
@@ -13,3 +15,14 @@ links.forEach(link => {
     burger.classList.toggle("toggle");
   });
 });
+
+
+  activities.addEventListener('change', (event) => {
+    if (event.target.value=="1"){
+      video1.css('display','flex');
+
+    }
+    else if(event.target.value=="0"){
+      video1.hide();
+    }
+  });
