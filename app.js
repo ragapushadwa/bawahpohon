87f14ced6a9986c7bff5034a8e8899d8af3ffd43
Raga Pushadwa <ragapushadwa@gmail.com>
2020-08-13 11:04:09 +0700

    dor

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const nav = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
 const links = nav.querySelectorAll("a");
 let video1 = $(".gallerybox");
+let video2 = $(".gallerybox1");
+let video3 = $(".gallerybox2");
+let video4 = $(".gallerybox3");
 var activities=document.getElementById("kotak1");
 
 burger.addEventListener("click", () => {
@@ -20,9 +23,33 @@ links.forEach(link => {
   activities.addEventListener('change', (event) => {
     if (event.target.value=="1"){
       video1.css('display','flex');
+      video2.hide();
+      video3.hide();
+      video4.hide();
 
     }
-    else if(event.target.value=="0"){
+    else if(event.target.value=="2"){
+      video2.css('display','flex');
       video1.hide();
+      video3.hide();
+      video4.hide();
+    }
+    else if(event.target.value=="3"){
+      video3.css('display','flex');
+      video2.hide();
+      video1.hide();
+      video4.hide();
+    }
+    else if(event.target.value=="4"){
+      video4.css('display','flex');
+      video2.hide();
+      video3.hide();
+      video1.hide();
+    }
+    else{
+      video4.hide();
+      video2.hide();
+      video3.hide();
+      video1.hide(); 
     }
   });
